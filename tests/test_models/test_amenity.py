bf3ae15ece83c60a05000e46fa5e0f6d2bd5956f
MMhenna <mhenna.mehdi@gmail.com>
2021-07-01 01:05:46 +0100

    commit

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,64 +1,37 @@
 #!/usr/bin/python3
-"""Unit tests for class Amenity"""
+
+'''
+    All the test for the amenity model are implemented here.
+'''
+
 import unittest
-import pep8
-import os
 from models.base_model import BaseModel
 from models.amenity import Amenity
 
 
-class TestAdditionalClasses(unittest.TestCase):
-    """testing class Amenity"""
-    @classmethod
-    def setUp(cls):
-        """setup instance"""
-        cls.a1 = Amenity()
-        cls.a1.name = "Gym"
+class TestAmenity(unittest.TestCase):
+    '''
+        Testing Amenity class
+    '''
 
-    @classmethod
-    def tearDown(cls):
-        """delete instance"""
-        del cls.a1
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
+    def test_Amenity_inheritence(self):
+        '''
+            tests that the Amenity class Inherits from BaseModel
+        '''
+        new_amenity = Amenity()
+        self.assertIsInstance(new_amenity, BaseModel)
 
-    def test_style_check(self):
-        """tests pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/amenity.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_Amenity_attributes(self):
+        '''
+            Test that Amenity class had name attribute.
+        '''
+        new_amenity = Amenity()
+        self.assertTrue("name" in new_amenity.__dir__())
 
-    def test_is_subclass(self):
-        """check that class of instance is a subclass of BaseModel"""
-        self.assertTrue(issubclass(self.a1.__class__, BaseModel), True)
-
-    def test_checking_for_functions(self):
-        """check docstrings for existing functions"""
-        self.assertIsNotNone(Amenity.__doc__)
-
-    def test_has_attributes(self):
-        """check that instance has all class attributes"""
-        self.assertTrue('id' in self.a1.__dict__)
-        self.assertTrue('created_at' in self.a1.__dict__)
-        self.assertTrue('updated_at' in self.a1.__dict__)
-        self.assertTrue('name' in self.a1.__dict__)
-
-    def test_attributes_type(self):
-        """check that all class attribute have appropriate values"""
-        self.assertIsInstance(self.a1, Amenity)
-        self.assertIsInstance(self.a1, BaseModel)
-        self.assertIsInstance(self.a1.name, str)
-
-    def test_save(self):
-        """check save method"""
-        self.a1.save()
-        self.assertNotEqual(self.a1.created_at, self.a1.updated_at)
-
-    def test_to_dict(self):
-        """check to_dict method"""
-        self.assertEqual('to_dict' in dir(self.a1), True)
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_Amenity_attribute_type(self):
+        '''
+            Test that Amenity class had name attribute's type.
+        '''
+        new_amenity = Amenity()
+        name_value = getattr(new_amenity, "name")
+        self.assertIsInstance(name_value, str)
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,68 +1,45 @@
 #!/usr/bin/python3
-"""Unit tests for class Review"""
+
+'''
+    All the test for the user model are implemented here.
+'''
+
 import unittest
-import os
-import pep8
 from models.base_model import BaseModel
 from models.review import Review
 
 
-class TestAmenity(unittest.TestCase):
-    """testing class Amenity"""
-    @classmethod
-    def setUp(cls):
-        """setup instance"""
-        cls.r1 = Review()
-        cls.r1.place_id = "Raleigh"
-        cls.r1.user_id = "Greg"
-        cls.r1.text = "Grade A"
+class TestReview(unittest.TestCase):
+    '''
+        Testing Review class
+    '''
 
-    @classmethod
-    def tearDownClass(cls):
-        """delete instance"""
-        del cls.r1
-        try:
-            os.remove("file.json")
-        except FileNotFoundError:
-            pass
+    def test_Review_inheritance(self):
+        '''
+            tests that the Review class Inherits from BaseModel
+        '''
+        new_review = Review()
+        self.assertIsInstance(new_review, BaseModel)
 
-    def test_style_check(self):
-        """test pep8 style"""
-        style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/review.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+    def test_Review_attributes(self):
+        '''
+            Test that Review class has place_id, user_id and text
+            attributes.
+        '''
+        new_review = Review()
+        self.assertTrue("place_id" in new_review.__dir__())
+        self.assertTrue("user_id" in new_review.__dir__())
+        self.assertTrue("text" in new_review.__dir__())
 
-    def test_is_subclass(self):
-        """check that class of instance is a subclass of BaseModel"""
-        self.assertTrue(issubclass(self.r1.__class__, BaseModel), True)
-
-    def test_checking_for_functions(self):
-        """check docstrings for existing functions"""
-        self.assertIsNotNone(Review.__doc__)
-
-    def test_has_attributes(self):
-        """check that instance has all class attributes"""
-        self.assertTrue('id' in self.r1.__dict__)
-        self.assertTrue('created_at' in self.r1.__dict__)
-        self.assertTrue('updated_at' in self.r1.__dict__)
-        self.assertTrue('place_id' in self.r1.__dict__)
-        self.assertTrue('text' in self.r1.__dict__)
-        self.assertTrue('user_id' in self.r1.__dict__)
-
-    def test_attributes_types(self):
-        """check that all class attribute have appropriate values"""
-        self.assertIsInstance(self.r1.place_id, str)
-        self.assertIsInstance(self.r1.user_id, str)
-        self.assertIsInstance(self.r1.text, str)
-
-    def test_save(self):
-        """check save method"""
-        self.r1.save()
-        self.assertNotEqual(self.r1.created_at, self.r1.updated_at)
-
-    def test_to_dict(self):
-        """check to_dict method"""
-        self.assertEqual('to_dict' in dir(self.r1), True)
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_Review_attributes(self):
+        '''
+            Test that Review class has place_id, user_id and text
+            attributes.
+        '''
+        new_review = Review()
+        place_id = getattr(new_review, "place_id")
+        user_id = getattr(new_review, "user_id")
+        text = getattr(new_review, "text")
+        self.assertIsInstance(place_id, str)
+        self.assertIsInstance(user_id, str)
+        self.assertIsInstance(text, str)
